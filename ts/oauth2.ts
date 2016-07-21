e071e954439df728a31ac7c23367b1adbc38734b
wchang28 <wchang28@hotmail.com>
2016-07-21 15:20:51 -0400

    fbgfmbnsfk

--- ts/oauth2.ts
+++ ts/oauth2.ts
@@ -32,3 +32,54 @@ export interface IAuthorizationWorkflowParams {
 	redirect_uri: string;
 	state?: string;    
 }
+
+export class TokenGrant {
+    constructor(private jQuery:any, public tokenGrantUrl:string, public clientAppSettings:IClientAppSettings) {}
+    getAccessTokenFromAuthCode (code:string, done:(err:any, access: Access) => void) : void {
+        let params: ITokenGrantParams = {
+            grant_type: 'authorization_code'
+            ,code: code
+            ,client_id: this.clientAppSettings.client_id
+            ,client_secret: this.clientAppSettings.client_secret
+            ,redirect_uri: this.clientAppSettings.redirect_uri
+        };
+        this.jQuery.post(this.tokenGrantUrl, params)
+        .done((data) => {
+            let access:Access = JSON.parse(data);
+            if (typeof done === 'function') done(null, access);
+        }).fail((err) => {
+            if (typeof done === 'function') done(err, null);
+        });		
+    }
+    getAccessTokenFromPassword(username:string, password:string, done:(err:any, access: Access) => void) : void {
+        let params: ITokenGrantParams = {
+            grant_type: 'password'
+            ,client_id: this.clientAppSettings.client_id
+            ,client_secret: this.clientAppSettings.client_secret
+            ,username: username
+            ,password: password
+        };
+        this.jQuery.post(this.tokenGrantUrl, params)
+        .done((data) => {
+            let access: Access = JSON.parse(data);
+            if (typeof done === 'function') done(null, access);
+        }).fail((err) => {
+            if (typeof done === 'function') done(err, null);
+        });			
+    };
+    refreshAccessToken(refresh_token:string, done:(err:any, access: Access) => void) : void {
+        let params: ITokenGrantParams = {
+            grant_type: 'refresh_token'
+            ,client_id: this.clientAppSettings.client_id
+            ,client_secret: this.clientAppSettings.client_secret
+            ,refresh_token: refresh_token
+        };
+        this.jQuery.post(this.tokenGrantUrl, params)
+        .done((data) => {
+            let access:Access = JSON.parse(data);
+            if (typeof done === 'function') done(null, access);
+        }).fail((err) => {
+            if (typeof done === 'function') done(err, null);
+        });		
+    };
+} 
