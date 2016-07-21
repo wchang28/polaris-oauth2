a29580a4e5f32b187c27ceb3b0a024ae441fcc9e
wchang28 <wchang28@hotmail.com>
2016-07-21 15:31:01 -0400

    hxgnghmdhj

--- ts/clientAppAuthEndPoint.ts
+++ ts/clientAppAuthEndPoint.ts
@@ -1,7 +1,7 @@
 import {getAJaxon} from 'ajaxon';
 let $J = getAJaxon(require('jquery-no-dom'));
 import {IAuthorizeEndpointOptions} from "./appConfig";
-import {ClientAppSettings, AuthResponseType, AccessToken, Access} from './oauth2';
+import * as oauth2 from './oauth2';
 
 export interface IConnectedApp {
 	name: string;
@@ -15,7 +15,7 @@ export interface IUser {
 }
 
 export class ClientAppAuthEndPoint {
-	constructor (private options:IAuthorizeEndpointOptions, public clientAppSettings:ClientAppSettings) {}
+	constructor (private options:IAuthorizeEndpointOptions, public clientAppSettings:oauth2.ClientAppSettings) {}
 	get redirect_uri():string {return this.clientAppSettings.redirect_uri;}
 	getError(httpErr) {
 		if (httpErr) {
@@ -45,25 +45,25 @@ export class ClientAppAuthEndPoint {
 			if (typeof done === 'function') done(this.getError(err), connectedApp);
 		});
 	}
-	userLogin(response_type:AuthResponseType, requireClientSecret: boolean, requireRedirectUrl: boolean, username:string, password:string, signUpUserForApp:boolean, done:(err:any, ret:any) => void) {
+	userLogin(response_type:oauth2.AuthResponseType, requireClientSecret: boolean, requireRedirectUrl: boolean, username:string, password:string, signUpUserForApp:boolean, done:(err:any, ret:any) => void) {
 		let data = {'response_type' : response_type, 'requireClientSecret': requireClientSecret, 'requireRedirectUrl': requireRedirectUrl, 'username': username, 'password': password, 'signUpUserForApp': signUpUserForApp};
 		this.$P("/services/authorize/login", data, (err, ret) => {
 			if (typeof done === 'function') done(this.getError(err), ret);
 		});
 	};
-	getAccessFromAuthCode(code:string, done:(err:any, access:Access) => void) {
+	getAccessFromAuthCode(code:string, done:(err:any, access:oauth2.Access) => void) {
 		let data = {'code' : code};
 		this.$P("/services/authorize/get_access_from_auth_code", data, (err, access) => {
 			if (typeof done === 'function') done(this.getError(err), access);
 		});
 	};
-	refreshToken(refresh_token:string, done:(err:any, access:Access) => void) {
+	refreshToken(refresh_token:string, done:(err:any, access:oauth2.Access) => void) {
 		let data = {'refresh_token' : refresh_token};
 		this.$P("/services/authorize/refresh_token", data, (err, access) => {
 			if (typeof done === 'function') done(this.getError(err), access);
 		});
 	};
-	verifyAccessToken(accessToken: AccessToken, done:(err:any, user:IUser) => void) {
+	verifyAccessToken(accessToken: oauth2.AccessToken, done:(err:any, user:IUser) => void) {
 		let data = accessToken;
 		this.$P("/services/authorize/verify_token", data, (err, user) => {
 			if (typeof done === 'function') done(this.getError(err), user);
