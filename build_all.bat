213c2add362cc913c30ce649530660ecf5dbbb4a
wchang28 <wchang28@hotmail.com>
2016-07-22 12:47:47 -0400

    rgerag

--- build_all.bat
+++ build_all.bat
@@ -1,3 +1,2 @@
 @echo off
-call ".\node_modules\.bin\babel.cmd" --presets react,es2015 .\ui\babel -d .\ui\js
-call ".\node_modules\.bin\browserify.cmd" .\ui\js\main.js -o .\login\js\bundle.js
+call ".\node_modules\.bin\browserify.cmd" .\js\ui\main.js -o .\login\js\bundle.js
