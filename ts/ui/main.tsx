c45c22264ecb5dec468b6ff94337a763173b8a4e
wchang28 <wchang28@hotmail.com>
2016-07-25 17:13:45 -0400

    ghngdhyjt

--- ts/ui/main.tsx
+++ ts/ui/main.tsx
@@ -78,7 +78,7 @@ var Login = React.createClass({
 					<p><label>Sign in with your {appSettings.companyName} account</label></p>
 					<p><input className="w3-input" type="text" name="username" placeholder="Email" value={this.state.username} onChange={this.handleUsernameChange}/></p>
 					<p><input className="w3-input" type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}/></p>
-					<p><button className="w3-btn w3-white w3-border w3-border-blue w3-round" onClick={this.handleSubmit.bind(this)}>Sign in</button></p>
+					<p><button className="w3-btn w3-white w3-border w3-border-blue w3-round" onClick={this.handleSubmit}>Sign in</button></p>
 				</form>
 				<div className="w3-container w3-white">
 					<p><a href="#reset_password" style={forgetMyPasswordStyle}>I forgot my password</a></p>
@@ -433,7 +433,7 @@ let $P = get$P(getParameterByName('p'));
 $P('/services/client/get_connected_app', {}, (err:any, connectedApp: IConnectedApp) => {
 	if (!err) {
 		let mode = "login";
-		ReactDOM.render(<OAuth2LoginApp mode={mode} connectedApp={connectedApp}/>, document.getElementById('main'));
+		ReactDOM.render(<OAuth2LoginApp mode={mode} connectedApp={connectedApp} $P={$P}/>, document.getElementById('main'));
 		$(window).on('hashchange', function() {
 			//alert('hash change');
 			mode = (window.location.hash.length > 0 ? window.location.hash.substr(1) : 'login');
